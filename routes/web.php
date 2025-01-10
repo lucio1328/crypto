ce8a90cee067849e608849198f2afe78281391b1
AlainPatrick003 <152412702+AlainPatrick003@users.noreply.github.com>
2025-01-10 11:23:59 +0300

    merge ok

--- routes/web.php
+++ routes/web.php
@@ -2,13 +2,15 @@
 
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\PortefeuilleController;
+use App\Http\Controllers\UtilisateurController;
 
 Route::get('/', function () {
-    return view('layout');
+    return view('inscription');
 });
 Route::get('formPortefeuille', function () {
     return view('insertionProtefeuille');
 });
+Route::post('/inscription', [UtilisateurController::class, 'inscription'])->name('inscription');
 Route::get('/portefeuille/form', [PortefeuilleController::class, 'form']);
 Route::get('/portefeuille/list', [PortefeuilleController::class, 'list']);
 Route::post('/portefeuille/create', [PortefeuilleController::class, 'create']);
